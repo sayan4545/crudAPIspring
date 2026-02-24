aab79ecc0c67861b01070dfb38bcd08811e9b03f
sayan4545 <sayanchatterjee3010@gmail.com>
2026-02-25 02:20:14 +0530

    Added Dotenv to the main class

--- src/main/java/com/dev/sayan/algocamp/gradledeepdive/crudapi/CrudApiApplication.java
+++ src/main/java/com/dev/sayan/algocamp/gradledeepdive/crudapi/CrudApiApplication.java
@@ -1,17 +1,20 @@
 package com.dev.sayan.algocamp.gradledeepdive.crudapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan
-@SpringBootConfiguration
+@SpringBootApplication
 public class CrudApiApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries()
+                .forEach(dotenvEntry -> System.setProperty(dotenvEntry.getKey(),dotenvEntry.getValue()));
+
         SpringApplication.run(CrudApiApplication.class, args);
     }
 
